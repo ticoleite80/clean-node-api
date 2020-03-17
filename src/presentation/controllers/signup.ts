fc5bf1f2ea99defcf949487b3dc30ff207bac660
C1292960 <C1292960@interno.bb.com.br>
2020-03-17 08:34:28 -0300

    refactor: task XXXXXXX - simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const element of requiredFields) {
+      if (!httpRequest.body[element]) {
+        return badRequest(new MissingParamError(element))
+      }
     }
   }
 }
