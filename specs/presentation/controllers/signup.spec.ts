902e9b8424e81811288cefa200bd42b9d45f81e1
C1292960 <C1292960@interno.bb.com.br>
2020-03-17 09:35:30 -0300

    test: task XXXXXXX - ensure SignupController returns 500 if EmailValidator throws

--- specs/presentation/controllers/signup.spec.ts
+++ specs/presentation/controllers/signup.spec.ts
@@ -1,6 +1,7 @@
 import { SignupController } from '../../../src/presentation/controllers/signup'
 import { MissingParamError } from '../../../src/presentation/erros/missing-param-error'
 import { InvalidParamError } from '../../../src/presentation/erros/invalid-param-error'
+import { ServerError } from '../../../src/presentation/erros/server-error'
 import { EmailValidator } from '../../../src/presentation/protocols/email-validator'
 
 interface SutTypes {
@@ -108,5 +109,26 @@ describe('Signup Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com')
+  })
+
+  test('Shoud return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignupController(emailValidatorStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
